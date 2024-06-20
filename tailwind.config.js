b18a69937ebb1373992c9bfe5c269b91bd7c401a
Marcio Gabriel <marciogabriel.gs27@gmail.com>
2024-06-20 08:57:17 -0300

    web-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    './src/**/*.{js,jsx,ts,tsx}',"./index.html",flowbite.content(),
+  ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'bole': '#764134',
-      'gold': '#C8AD55',
-      'sienna': '#554742',
-      'feld': '#5B6C5D',
-      'cor1': '#332E2C',
-      'laranja': '#F55318',
-      'cinza': '#91837E',
-      'chamoisee': '#B08968',
-      'coffe': '#7F5539',
-      'umber': '#9C6644',
-      'dun': '#E6CCB2',
-      'tan': '#DDB892',
-
-
-
-
-    },
     extend: {},
   },
   plugins: [
-    require('tailwindcss-animated')
+    require('flowbite/plugin'),
+    flowbite.plugin(),
   ],
 }
 
